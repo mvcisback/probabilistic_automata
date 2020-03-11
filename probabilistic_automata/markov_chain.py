fd9d645875d9c5382e56c7d72eb8fdb600f2a608
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-03-11 12:38:52 -0700

    Allow Markov chains to estimate trace likelihoods.

--- probabilistic_automata/markov_chain.py
+++ probabilistic_automata/markov_chain.py
@@ -1,6 +1,11 @@
+from typing import Sequence
+
 import attr
 
-from probabilistic_automata.pa import PDFA
+from probabilistic_automata.pa import PDFA, State
+
+
+Trace = Sequence[State]
 
 
 @attr.s(frozen=True, auto_attribs=True)
@@ -16,6 +21,19 @@ class MarkovChain(PDFA):
                 "Number of system inputs != 1\n"
                 "Underlying system potentially non-determinstic."
             )
+
+    def trace_prob(self, trc: Trace) -> float:
+        """
+        Compute the probability of this markov chain generating the
+        provided trace.
+        """
+        sys_input, *_ = self.inputs
+        prev_state, prob = self.start, 1
+        for state in trc:
+            prob *= self.prob(prev_state, state, sys_input)
+            if prob == 0:
+                return 0
+        return prob
 
 
 def from_pdfa(pdfa: PDFA) -> MarkovChain:
--- tests/test_markov_chain.py
+++ tests/test_markov_chain.py
@@ -5,14 +5,25 @@ from probabilistic_automata import pa as PA
 from probabilistic_automata import markov_chain as MC
 
 
+SYS = DFA(
+    start=0, inputs={0, 1},
+    label=lambda s: s == 2,
+    transition=lambda s, c: (s + c) % 4,
+)
+
+
 def test_validator():
-    sys = DFA(
-        start=0, inputs={0, 1},
-        label=lambda s: s == 2,
-        transition=lambda s, c: s + sum(c) % 4,
-    )
-
     with pytest.raises(ValueError):
-        MC.from_pdfa(PA.lift(sys))   # Non-determinism.
+        MC.from_pdfa(PA.lift(SYS))   # Non-determinism.
 
-    MC.from_pdfa(PA.randomize(sys))  # Actually Markov Chain.
+    MC.from_pdfa(PA.randomize(SYS))  # Actually Markov Chain.
+
+
+def test_trace_prob():
+    mc1 = MC.from_pdfa(PA.randomize(SYS))
+
+    trc = 3*[0]
+    assert pytest.approx(2**-3) == mc1.trace_prob(trc)
+
+    trc = 3*[1, 2, 1]
+    assert 0 == mc1.trace_prob(trc)
